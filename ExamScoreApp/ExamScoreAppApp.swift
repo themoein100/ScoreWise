cf9445d6b9bfd327c4d6a229aaa5ada1266348a6
themoein100 <2002.sadraei@gmail.com>
2025-05-23 13:53:40 +0330

    Initial Commit

---
+++ ExamScoreApp/ExamScoreAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExamScoreAppApp.swift
+//  ExamScoreApp
+//
+//  Created by MoeinSadraei on 5/23/25.
+//
+
+import SwiftUI
+
+@main
+struct ExamScoreAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ExamScoreAppTests/ExamScoreAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  ExamScoreAppTests.swift
+//  ExamScoreAppTests
+//
+//  Created by MoeinSadraei on 5/23/25.
+//
+
+import Testing
+@testable import ExamScoreApp
+
+struct ExamScoreAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
